cc9306f508dd017a785e383121f3f31293d80b03
Huann Almeida <huannvictor@gmail.com>
2023-03-27 22:21:14 -0300

    projeto iniciado

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "1234",
+  database: "controle-rcn",
+  define: {
+    timestamps: true,
+  },
+};
