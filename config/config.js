fdfd41dd80fe3340cf556b30670d07ee38839140
Huann Almeida <huannvictor@gmail.com>
2023-03-31 11:55:52 -0300

    host alterado

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     username: "DB_USERNAME",
     password: "DB_PASSWORD",
     database: "controle-rcn",
-    host: "localhost",
+    host: "127.0.0.1:5432",
     dialect: "postgres",
   },
   test: {
