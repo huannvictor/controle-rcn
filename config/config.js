dfa9e07ebeafe98f89879de57d3d8124cbedbf55
Huann Almeida <huannvictor@gmail.com>
2023-03-31 10:34:32 -0300

    build

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   dialect: "postgres",
   host: "localhost",
@@ -6,5 +8,26 @@ module.exports = {
   database: "controle-rcn",
   define: {
     timestamps: true,
+  },
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
 };
